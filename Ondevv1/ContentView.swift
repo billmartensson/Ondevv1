6efa0766620cf582a85e3cda496b628426e0305b
Bill Mårtensson <bill@magictechnology.se>
2024-10-16 14:08:45 +0200

    start

--- Ondevv1/ContentView.swift
+++ Ondevv1/ContentView.swift
@@ -7,17 +7,70 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+
+    @State var counter = 0
+    
+    
     var body: some View {
+        
+        
         VStack {
+            
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            
+            Text("Tjena, world!")
+            
+            if counter > 5 {
+                Text("MÅNGA KLICK!!!")
+                    .foregroundColor(Color.white)
+                    .frame(width: 200.0, height: 50.0)
+                    .background(Color.red)
+            }
+            
+            
+            Text("Klick \(counter) gånger")
+                .font(.largeTitle)
+            
+            Button("Klicka här") {
+                counter = counter + 1
+            }
+            
+            HStack {
+                VStack {
+                    
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.red)
+                
+                VStack {
+                    
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.blue)
+
+            }
+            .frame(maxWidth: .infinity, maxHeight: 200)
+            .background(Color.yellow)
+
         }
-        .padding()
+        
+        
+        
     }
+    
+    
+    
 }
+
+
+
 
 #Preview {
     ContentView()
