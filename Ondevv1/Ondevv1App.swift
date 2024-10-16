52a73c337da4a312d4b17d6c77372bd3f2928762
Bill Mårtensson <bill@magictechnology.se>
2024-10-16 13:17:37 +0200

    Initial Commit

---
+++ Ondevv1/Ondevv1App.swift
@@ -0,0 +1,17 @@
+//
+//  Ondevv1App.swift
+//  Ondevv1
+//
+//  Created by BillU on 2024-10-16.
+//
+
+import SwiftUI
+
+@main
+struct Ondevv1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ondevv1Tests/Ondevv1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Ondevv1Tests.swift
+//  Ondevv1Tests
+//
+//  Created by BillU on 2024-10-16.
+//
+
+import Testing
+@testable import Ondevv1
+
+struct Ondevv1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
